159dfef6654dbd1071382f9a6747aca7c9778670
Guóth Kevin <gkevin94@gmail.com>
2018-11-23 22:09:08 +0100

    add calculator2 html and js

---
+++ js/calculator2.js
@@ -0,0 +1,9 @@
+function nine() {
+    9 = document.getElementById('number1').innerText;
+}
+
+
+function eredmeny() {
+    var szam1 = parseFloat(document.getElementById('number1').value);
+    var szam2 = parseFloat(document.getElementById('number2').value);
+}
