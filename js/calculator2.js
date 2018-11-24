4e7ede0e5728ca09bfd473ef75a2326514054cec
Guóth Kevin <gkevin94@gmail.com>
2018-11-24 16:31:45 +0100

    updated calculator2.html and .js

--- js/calculator2.js
+++ js/calculator2.js
@@ -1,9 +1,57 @@
+var result = document.getElementById('disp1');
+var symbol = document.getElementById('symb');
+var fistNumberBox = document.getElementById('disp1');
+
 function nine() {
-    9 = document.getElementById('number1').innerText;
+  result.value += 9;
+}
+function eight() {
+  result.value += 8;
+}
+function seven() {
+  result.value += 7;
+}
+function six() {
+  result.value += 6;
+}
+function five() {
+  result.value += 5;
+}
+function four() {
+  result.value += 4;
+}
+function three() {
+  result.value += 3;
+}
+function two() {
+  result.value += 2;
+}
+function one() {
+  result.value += 1;
+}
+function zero() {
+  result.value += 0;
+}
+function point() {
+  result.value += '.';
+}
+function clear() {
+  result.value === 'empty';
 }
 
-
+function div() {
+  symbol.value = '/';
+}
+function mul() {
+  symbol.value = '*';
+}
+function sub() {
+  symbol.value = '-';
+}
+function add() {
+  symbol.value = '+';
+}
 function eredmeny() {
-    var szam1 = parseFloat(document.getElementById('number1').value);
-    var szam2 = parseFloat(document.getElementById('number2').value);
+  var szam1 = parseFloat(document.getElementById('number1').value);
+  var szam2 = parseFloat(document.getElementById('number2').value);
 }
