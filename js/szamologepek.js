6ebb7dc6c439d0519c5a7a4fe0253f116778369f
Anita Adam <anita.aadam@gmail.com>
2018-11-25 16:14:37 +0100

    updated szamologepek.html

--- js/szamologepek.js
+++ js/szamologepek.js
@@ -1,20 +1,37 @@
-function calculate() {
-  var num1 = parseFloat(document.getElementById('num1').value);
-  var num2 = parseFloat(document.getElementById('num2').value);
-  var action = document.getElementById('muvelet').value;
+function anitaInsert(num) {
+  var operatorValueDefault =
+    document.querySelector('#anitaOperator').value === 'default';
+  if (operatorValueDefault === true) {
+    document.querySelector('#anitaNum1').value += num;
+  } else if (operatorValueDefault === false) {
+    document.querySelector('#anitaNum2').value += num;
+  }
+}
 
-  if (action === '+') {
-    document.getElementById('eredmeny').value = num1 + num2;
+function anitaCalculate() {
+  var anitaOperator = document.querySelector('#anitaOperator').value;
+  var aNum1 = parseFloat(document.querySelector('#anitaNum1').value, 10);
+  var aNum2 = parseFloat(document.querySelector('#anitaNum2').value, 10);
+  var anitaEredmeny;
+
+  if (anitaOperator === '+') {
+    anitaEredmeny = aNum1 + aNum2;
+  } else if (anitaOperator === '-') {
+    anitaEredmeny = aNum1 - aNum2;
+  } else if (anitaOperator === '*') {
+    anitaEredmeny = aNum1 * aNum2;
+  } else if (anitaOperator === '/') {
+    anitaEredmeny = aNum1 / aNum2;
+  } else if (anitaOperator === '%') {
+    anitaEredmeny = aNum1 % aNum2;
+  } else if (anitaOperator === '**') {
+    anitaEredmeny = aNum1 ** aNum2;
   }
-  if (action === '-') {
-    document.getElementById('eredmeny').value = num1 - num2;
-  }
-  if (action === '*') {
-    document.getElementById('eredmeny').value = num1 * num2;
-  }
-  if (action === '/') {
-    document.getElementById('eredmeny').value = num1 / num2;
-  }
+
+  document.querySelector('#anitaResult').value = anitaEredmeny;
+  document.querySelector('#anitaOperator').value = 'default';
+  document.querySelector('#anitaNum1').value = '';
+  document.querySelector('#anitaNum2').value = '';
 }
 
 
