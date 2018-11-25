fa704e23361ba0cd3fe7e97d1b7659f917469b36
Guóth Kevin <gkevin94@gmail.com>
2018-11-25 18:15:46 +0100

    changed csapat.html and show.js and added kevin calc to szgepek.html/js

--- js/szamologepek.js
+++ js/szamologepek.js
@@ -74,3 +74,75 @@ function calc() {
     document.getElementById('result').value = firstnumber * secondnumber;
   }
 }
+
+var numbers = document.getElementById('ans');
+var result = document.getElementById('result');
+
+function cler() {
+  numbers.value = null;
+  result.value = null;
+}
+
+function one() {
+  numbers.value += 1;
+}
+
+function two() {
+  numbers.value += 2;
+}
+
+function three() {
+  numbers.value += 3;
+}
+
+function four() {
+  numbers.value += 4;
+}
+
+function five() {
+  numbers.value += 5;
+}
+
+function six() {
+  numbers.value += 6;
+}
+
+function seven() {
+  numbers.value += 7;
+}
+
+function eight() {
+  numbers.value += 8;
+}
+
+function nine() {
+  numbers.value += 9;
+}
+
+function zero() {
+  numbers.value += 0;
+}
+
+function sub() {
+  numbers.value += '-';
+}
+
+function add() {
+  numbers.value += '+';
+}
+
+function mul() {
+  numbers.value += '*';
+}
+
+function divide() {
+  numbers.value += '/';
+}
+
+function point() {
+  numbers.value += '.';
+}
+
+function equal() {
+  result.value = eval(numbers.value);
+}
